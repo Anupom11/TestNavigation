314cea6f71dc5dfd11b2f9c179bfdc217745846e
Anupom11 <anupom.chakrabarty@gmail.com>
2022-11-22 11:21:47 +0530

    Updated with drawer navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer, TabActions } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import 'react-native-gesture-handler';
 
 function HomeScreen({navigation}) {
   return (
